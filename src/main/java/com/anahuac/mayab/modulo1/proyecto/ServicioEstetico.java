e21a2175146d389a0b69a32f49babd9707c2ef41
Sis <Sis@Sistemas>
2024-11-29 20:15:18 -0600

    er

---
+++ src/main/java/com/anahuac/mayab/modulo1/proyecto/ServicioEstetico.java
@@ -0,0 +1,29 @@
+package com.anahuac.mayab.modulo1.proyecto;
+
+public class ServicioEstetico extends Servicio{
+
+	public ServicioEstetico(String tipo, double costo, String fecha, String encargado) {
+		super(tipo, costo, fecha, encargado);
+		
+	}
+
+
+	public void baniar() {
+		System.out.println ("Bañando.....");
+	
+	}
+	public void cepillar() {
+		System.out.println ("Cepillando.....");
+	}
+	public void cortarUnias() {
+		System.out.println ("Cortando uñas.....");	
+	}
+	public void realizar(String nombre) {
+		System.out.println ("Atendiendo a : " + nombre);
+		baniar();
+		cepillar();
+		cortarUnias();
+		
+	}
+
+}
