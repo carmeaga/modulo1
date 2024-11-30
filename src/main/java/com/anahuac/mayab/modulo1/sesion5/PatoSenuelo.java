ac4c108dc8a561e46149db248867e3647f817ad5
Sis <Sis@Sistemas>
2024-11-29 20:13:56 -0600

    +

---
+++ src/main/java/com/anahuac/mayab/modulo1/sesion5/PatoSenuelo.java
@@ -0,0 +1,22 @@
+package com.anahuac.mayab.modulo1.sesion5;
+
+public class PatoSenuelo extends Pato {
+
+	public PatoSenuelo(String nombre, String color) {
+		super(nombre, color);
+		// TODO Auto-generated constructor stub
+	}
+	public void comer() {
+		System.out.println ("soy un pato de señuelo y no puedo comer");
+	}
+	public void volar() {
+		System.out.println ("soy un pato de señuelo y no puedo volar");
+		
+}
+	public void quack() {
+		System.out.println ("quack quack");
+}
+}
+
+
+
