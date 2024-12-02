46cd5228cbb5f3c819006ac68a4237452bbd4a2e
Sis <Sis@Sistemas>
2024-12-02 17:48:49 -0600

    j

---
+++ src/main/java/com/anahuac/mayab/modulo1/sesion5/PatoMallard.java
@@ -0,0 +1,31 @@
+package com.anahuac.mayab.modulo1.sesion5;
+
+public class PatoMallard extends Pato{
+
+	public PatoMallard(String nombre, String color) {
+		super(nombre, color);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public void quack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/*public PatoMallard(String nombre, String color) {
+		super(nombre, color);*/
+		// TODO Auto-generated constructor stub
+	}
+	/*public void comer() {
+		System.out.println ("soy un pato de mallard y  puedo comer");
+	}
+	public void volar() {
+		System.out.println ("soy un pato de mallard y no puedo volar");
+		
+}
+	public void quack() {
+		System.out.println ("quack quack");*/
+
+
